3ea7f4cf02010350868605c198c74c1607ca79b2
BotenAqua <aquapelzacz@gmail.com>
2022-10-02 20:02:41 +0200

    add webpages generation using templates

--- project_files/__init__.py
+++ project_files/__init__.py
@@ -1,4 +1,4 @@
-from os import listdir
+from os import listdir, mkdir
 from os.path import isdir, isfile
 import yaml
 
@@ -6,15 +6,30 @@ from . import webpage_generator
 
 if not isfile('./config.yaml'):
     with open('config.yaml', 'w') as config_file:
-        config = [
-            {
-                'posts': {
-                    'posts_directory': './posts'
-                    }
-            }
-        ]
-        yaml.dump(config, config_file)
-    print('No config.yaml file found. New config file was generated.')
+        default_config = {
+            'config version': 0.1,
+            'input': {
+                'posts directory': './posts'
+                },
+            'output': {
+                'index file location': './',
+                'index file name': 'index.html',
+                'subpages location': './pages'
+                }
+        }
+        yaml.dump(default_config, config_file)
+    print('No config.yaml file found. New config file was generated. Please configure your blog and run the program again.')
     exit()
 
-print(webpage_generator.generate_page(content='Habababa!'))
+with open('config.yaml', 'r') as config_file:
+    config = yaml.load(config_file, yaml.FullLoader)
+
+# TODO: missing directories and files creation
+# if not isdir(config['input']['posts directory']):
+#     mkdir(config['input']['posts directory'])
+
+
+# generate index file
+with open('index.html', 'w') as index_file:
+    index_file.write(webpage_generator.generate_page(content='Habababa!'))
+print('Your blog was generated successfully!')
--- project_files/webpage_generator/__init__.py
+++ project_files/webpage_generator/__init__.py
@@ -1,5 +1,18 @@
-from jinja2 import Template
+from jinja2 import FileSystemLoader, Environment
 
-def generate_page(content):
-    page = Template('This is a test! The content you provided is: {{ page_content }}')
-    return page.render(page_content=content)
+file_loader = FileSystemLoader('./project_files/templates') # TODO: path changing
+
+env = Environment(loader=file_loader)
+env.trim_blocks = True
+env.lstrip_blocks = True
+env.rstrip_blocks = True
+
+from pathlib import Path
+mypath = Path().absolute()
+print('Absolute path : {}'.format(mypath))
+
+template = env.get_template('base.html')
+
+
+def generate_page(content='', title='Default title', language='en'):
+    return template.render(content=content)
